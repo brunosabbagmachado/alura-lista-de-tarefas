2094a4249169f92fd74b78d6ad59c7b25aa0bd31
Machado <brunosabbagmachado@outlook.com>
2021-09-22 07:20:08 -0300

    Implementando botao Novo item

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,4 +1,24 @@
-const novaTarefa = document.querySelector('[data-form-button]');
+const criarTarefa = (evento) => {
 
-novaTarefa.addEventListener('click', ()=> {
-    console.log('fui clicado')})
+  evento.preventDefault();
+
+  const lista = document.querySelector('[data-list]');
+  const input = document.querySelector("[data-form-input]");
+  
+  const valor = input.value;
+
+  const tarefa = document.createElement('li');
+  tarefa.classList.add('task');
+
+  const conteudo = `<p class="content">${valor}</p>`;
+
+  tarefa.innerHTML = conteudo;
+
+  lista.appendChild(tarefa);
+
+  input.value = " ";
+}
+
+const novaTarefa = document.querySelector("[data-form-button]");
+
+novaTarefa.addEventListener("click", criarTarefa);
